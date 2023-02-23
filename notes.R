98dc6bc29185cbd5b93b02108f6e5e33e5a20404
jonellevillar <jonellevillar@gmail.com>
2023-02-23 13:22:53 +0100

    update

---
+++ notes.R
@@ -0,0 +1,20 @@
+.title-slide.remark-slide-content:before {
+  position: absolute;
+  content: url(slides/NORMENT_LOGO_Utekst_hvit.png);
+  height: 40px;
+  
+  
+  .title-slide {
+    background-image: url(slides/NORMENT_LOGO_Utekst_hvit.png);
+    background-position: 100% 0%
+      background-size: 20px;
+    padding-left: 100px
+    
+    
+  }
+  
+  
+  .title-slide.remark-slide-content:after {
+    content: url(slides/NORMENT_LOGO_Utekst_hvit.png);
+    margin-bottom: 5px;
+  }  
